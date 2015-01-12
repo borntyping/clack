236880b9768b9e4c84f1ab9560c6e20932abb0fd
Sam Clements <sam@borntyping.co.uk>
2015-01-12 19:47:17 +0000

    Skip farm.json example if cowsay is not installed

--- tests/test_examples.py
+++ tests/test_examples.py
@@ -1,10 +1,15 @@
 """Test the examples in ``../examples``."""
 
+import subprocess
+
 import clack.cli
 
 import pytest
 
 
+@pytest.mark.skipif(
+    subprocess.call('which cowsay', shell=True),
+    reason="cowsay is not installed")
 def test_farm(run_example):
     assert run_example('farm').exit_code == 0
 
